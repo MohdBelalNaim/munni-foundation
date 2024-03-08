047e7a1150804e1e0fb9b6847916716901dff619
Tamhi <hirahudakhan20@gmail.com>
2024-03-08 19:18:59 +0530

    Changed text size

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -18,7 +18,7 @@ function Profile() {
       <div>
         <div className="lg:mx-36 relative rounded-xl overflow-hidden border border-gray-200 bg-white my-8 lg:pl-14 px-4 py-4 mx-4">
           <div className="flex justify-end lg:mr-8 absolute right-0">
-            <button className="bg-secondary text-white px-8 py-2 mx-4  rounded-full max-sm:hidden" onClick={()=>seteditProfile(!editProfile)}>
+            <button className="bg-secondary text-white px-8 py-2 mx-4 rounded-full max-sm:hidden" onClick={()=>seteditProfile(!editProfile)}>
               EDIT
             </button>
           </div>
@@ -32,45 +32,45 @@ function Profile() {
               className="rounded-full"
             />
             <div className="pt-8">
-              <p className="lg:text-lg text-gray-500">Name</p>
-              <p className="lg:text-xl">Sajad Ahmed</p>
+              <p className="xl:text-md text-gray-500 text-sm">Name</p>
+              <p className="xl:text-lg">Sajad Ahmed</p>
             </div>
           </div>
 
           <div className="flex lg:flex-row flex-col lg:gap-14 justify-between pr-14">
             <div>
               <div className="mb-4">
-                <p className="lg:text-lg text-gray-500">Email</p>
-                <p className="lg:text-xl">sajadkhaki09@gmail.com</p>
+                <p className="xl:text-md text-gray-500 text-sm">Email</p>
+                <p className="xl:text-lg">sajadkhaki09@gmail.com</p>
               </div>
               <div className="mb-4">
-                <p className="lg:text-lg text-gray-500">Phone Number</p>
-                <p className="lg:text-xl">+91-8379912482</p>
+                <p className="xl:text-md text-gray-500 text-sm">Phone Number</p>
+                <p className="xl:text-lg">+91-8379912482</p>
               </div>
               <div className="mb-4">
-                <p className="lg:text-lg text-gray-500">PAN Number</p>
-                <p className="lg:text-xl">BTLPA4771TA</p>
+                <p className="xl:text-md text-gray-500 text-sm">PAN Number</p>
+                <p className="xl:text-lg">BTLPA4771TA</p>
               </div>
             </div>
 
             <div>
               <div className="mb-4">
-                <p className="lg:text-lg text-gray-500">Date of Birth</p>
-                <p className="lg:text-xl">15-05-1997</p>
+                <p className="xl:text-md text-gray-500 text-sm">Date of Birth</p>
+                <p className="xl:text-lg">15-05-1997</p>
               </div>
               <div className="mb-4">
-                <p className="lg:text-lg text-gray-500">City of Residence</p>
-                <p className="lg:text-xl">Poonch, Jammu And Kashmir</p>
+                <p className="xl:text-md text-gray-500 text-sm">City of Residence</p>
+                <p className="xl:text-lg">Poonch, Jammu And Kashmir</p>
               </div>
               <div className="mb-4">
-                <p className="lg:text-lg text-gray-500">Aadhar Number</p>
-                <p className="lg:text-xl">**********2554</p>
+                <p className="xl:text-md text-gray-500 text-sm">Aadhar Number</p>
+                <p className="xl:text-lg">**********2554</p>
               </div>
             </div>
           </div>
 
           <div className="flex justify-center lg:hidden md:hidden">
-            <button className="bg-secondary text-white px-8 py-2 rounded-full">
+            <button className="bg-secondary text-sm text-white px-8 py-2 rounded-full">
               EDIT
             </button>
           </div>
@@ -79,29 +79,29 @@ function Profile() {
         <div className="flex lg:flex-row flex-col justify-between rounded-xl overflow-hidden border border-gray-200 bg-white lg:mx-36 mx-4 mb-8 lg:px-14 px-4 py-8">
           <div className="flex pb-8 lg:pb-0 gap-4 items-center">
             <div>
-              <FaDonate size={40} />
+              <FaDonate size={35} />
             </div>
             <div>
-              <p>-</p>
-              <p>Donations Made</p>
+              <p className="font-bold">-</p>
+              <p className="lg:text-md text-sm">Donations Made</p>
             </div>
           </div>
           <div className="flex pb-8 lg:pb-0 gap-4 items-center">
             <div>
-              <FaRupeeSign size={40} />
+              <FaRupeeSign size={35} />
             </div>
             <div>
-              <p>-</p>
-              <p>Last Donation Amount</p>
+              <p className="font-bold">-</p>
+              <p className="lg:text-md text-sm">Last Donation Amount</p>
             </div>
           </div>
           <div className="flex gap-4 items-center">
             <div>
-              <FaCalendar size={40} />
+              <FaCalendar size={35} />
             </div>
             <div>
-              <p>-</p>
-              <p>Last Donation Date</p>
+              <p className="font-bold">-</p>
+              <p className="lg:text-md text-sm">Last Donation Date</p>
             </div>
           </div>
         </div>
@@ -118,26 +118,26 @@ function Profile() {
                   />
                 </div>
                 <div className="xl:px-4 py-4">
-                  <p className="text-lg lg:text-xl">
+                  <p className="text-md ">
                     Help a farmer to feed and educate his 7 little girls
                   </p>
-                  <p className="py-2 text-xs md:text-sm lg:text-sm">
+                  <p className="py-2 text-xs ">
                     <a href="">Post An Update</a>
                   </p>
-                  <button className="bg-red-700 text-white px-8 py-1 rounded-lg">
+                  <button className="bg-red-700 text-sm text-white px-8 py-1.5 rounded-lg">
                     Expired
                   </button>
                 </div>
               </div>
 
               <div className="flex lg:gap-14 lg:flex-row flex-col justify-between">
-                <div className="flex gap-4 xl:px-4 my-4">
+                <div className="flex gap-2 xl:pr-4 my-4">
                   <div>
-                    <BiSolidDonateHeart size={50} />
+                    <BiSolidDonateHeart size={45} />
                   </div>
                   <div>
                     <p className="font-bold text-xl">0</p>
-                    <p className="text-sm">Fund Raised</p>
+                    <p className="text-xs">Fund Raised</p>
                   </div>
                 </div>
                 <div className="flex gap-4 xl:px-4 my-4 items-center">
@@ -146,16 +146,16 @@ function Profile() {
                   </div>
                   <div>
                     <p className="font-bold text-xl">0</p>
-                    <p className="text-sm">Days To Go</p>
+                    <p className="text-xs">Days To Go</p>
                   </div>
                 </div>
                 <div className="flex gap-4 xl:px-4 my-4 items-center">
                   <div>
-                    <FaEye size={45} />
+                    <FaEye size={40} />
                   </div>
                   <div>
                     <p className="font-bold text-xl">3</p>
-                    <p className="text-sm">Views</p>
+                    <p className="text-xs">Views</p>
                   </div>
                 </div>
               </div>
@@ -163,24 +163,24 @@ function Profile() {
 
             <div className="xl:w-2/5">
               <div class="flex lg:flex-row md:flex-row flex-col justify-center gap-5 pt-4">
-                <button class="bg-secondary text-white lg:text-md xl:text-lg py-2 text-sm rounded-full flex-grow">
+                <button class=" md:w-1/2 bg-secondary text-white lg:text-md py-2 text-sm rounded-full flex-grow">
                   PERFORMANCE
                 </button>
-                <button class="bg-secondary text-white lg:text-md xl:text-lg py-2 text-sm rounded-full flex-grow">
+                <button class=" md:w-1/2 bg-secondary text-white lg:text-md py-2 text-sm rounded-full flex-grow">
                   EDIT
                 </button>
               </div>
-              <div class="flex justify-center mt-2 mb-6">
-                <button class="bg-secondary text-white lg:text-md xl:text-lg py-2 text-sm rounded-full my-2 flex-grow">
+              <div class="flex justify-center mt-2 mb-2">
+                <button class="bg-secondary text-white lg:text-md py-2 text-sm rounded-full my-2 flex-grow">
                   UPLOAD DOCUMENTS
                 </button>
               </div>
 
               <hr />
-              <p className="my-2">
+              <p className="my-2 text-sm">
                 Keep sharing to increase your fundraiser search
               </p>
-              <div className="flex justify-between mb-8">
+              <div className="flex justify-between my-6">
                 <FaFacebook size={30} />
                 <FaInstagram size={30} />
                 <FaEnvelope size={30} />
@@ -193,7 +193,7 @@ function Profile() {
           <hr />
 
           <div className="flex justify-center mt-8">
-            <button className="bg-secondary lg:text-md xl:text-lg text-sm text-white px-14 py-2 rounded-full my-2">
+            <button className="bg-secondary lg:text-md text-sm text-white px-14 py-2 rounded-full mb-2">
               WITHDRAW FUNDS
             </button>
           </div>
