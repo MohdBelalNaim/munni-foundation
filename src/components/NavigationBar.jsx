f0b31d6b3b6491f4cc6a7765407dbe13630eee39
Tamhi <hirahudakhan20@gmail.com>
2024-03-10 12:35:30 +0530

    Added Edit Profile Pic Component and fixed some minor issues

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -13,25 +13,35 @@ function Navigation() {
             <img src="/logo.svg" alt="Ni load hua" className="w-[8%]" />
             <div className="text-sm">Munni Welfare Foundation</div>
           </div>
-          <div className="max-sm:hidden flex gap-8 md:gap-6 text-sm items-center">
-            <div>
-              <Link href="/">Home</Link>
-            </div>
-            <div>About</div>
-            <div>
-              <Link href="/contact">Contact</Link>
-            </div>
-            <div>Get Involved</div>
-            <div
-              onClick={() => setShowLogin(!showLogin)}
-              style={{ cursor: "pointer" }}
-            >
-              Login
-            </div>
-            <div className="text-secondary px-4 py-2 rounded-md text-xs  bg-primary flex items-center gap-2">
-              Donate <PiHandHeart className="text-xl" />
-            </div>
-          </div>
+          <div className="max-sm:hidden flex lg:gap-8 gap-4 text-sm items-center">
+                <div>
+                  <Link href="/">Home</Link>
+                </div>
+                <div>
+                  <Link href="/about">About</Link>
+                </div>
+                <div>
+                  <Link href="/contact">Contact</Link>
+                </div>
+                <div>Get Involved</div>
+                <div
+                  onClick={() => setShowLogin(!showLogin)}
+                  style={{ cursor: "pointer" }}
+                >
+                  Login
+                </div>
+                <div>
+                  <Link href="/profile">My Profile</Link>
+                </div>
+                <div>
+                  <Link href="/create-campaign">Create Campaign</Link>
+                </div>
+                <Link href="/campaign">
+                  <div className="text-secondary px-4 py-2 rounded-md text-xs  bg-primary flex items-center gap-2">
+                    Donate <PiHandHeart className="text-xl" />
+                  </div>
+                </Link>
+              </div>
         </div>
       </div>
 
